f8df3f82b1b33f4ab4601dcb02863a4fa93e3bb2
Andreas Mehlsen <andreasmehlsen@outlook.com>
2018-04-22 16:16:41 +0200

    Added more types to subject.

--- src/lib/subject.ts
+++ src/lib/subject.ts
@@ -1,22 +1,24 @@
+export declare type SubjectCallback<T> = (T) => void;
+
 /**
  * Allows observers to subscribe and unsubscribe to events and allows other entities to dispatch them.
  */
 export class Subject<T> {
-	protected listeners: ((T) => void)[] = [];
+	protected listeners: SubjectCallback<T>[] = [];
 
 	/**
 	 * Adds a listener to the subject.
-	 * @param {(T) => void} listener
+	 * @param listener
 	 */
-	addListener (listener: ((T) => void)) {
+	addListener (listener: SubjectCallback<T>) {
 		this.listeners.push(listener);
 	}
 
 	/**
 	 * Removes a listener from the subject.
-	 * @param {(T) => void} listener
+	 * @param listener
 	 */
-	removeListener (listener: ((T) => void)) {
+	removeListener (listener: SubjectCallback<T>) {
 		const matchIndex = this.listeners.indexOf(listener);
 		if (matchIndex >= 0) {
 			this.listeners.splice(matchIndex, 1);
